1283f35cd2cf9e057d3e76ce57cf239f69f353ac
Samuel <davidprieto123456782007@gmail.com>
2026-05-27 23:56:04 -0500

    fix: corrige menú y agrega opción 8 Salir

--- app_biblioteca_V1.py
+++ app_biblioteca_V1.py
@@ -136,12 +136,6 @@ def libro_mas_antiguo():
     print(f"Autor: {antiguo['autor']}")
     print(f"Año: {antiguo['año']}")
 
-# --------------------------------
-# Contar libros registrados
-# --------------------------------
-def contar_libros():
-
-    print(f"\nTotal de libros registrados: {len(biblioteca)}")
 
 # --------------------------------
 # MENU PRINCIPAL
@@ -155,8 +149,7 @@ while True:
     print("4. Buscar libros por autor")
     print("5. Eliminar libro")
     print("6. Mostrar libro más antiguo")
-    print("7. Contar libros registrados")
-    print("8. Salir")
+    print("7. Salir")
 
     opcion = input("Seleccione una opción: ")
 
@@ -180,11 +173,8 @@ while True:
         libro_mas_antiguo()
 
     elif opcion == "7":
-        contar_libros()
-
-    elif opcion == "8":
         print("Saliendo del programa...")
         break
 
     else:
-           print("Opción inválida.")
+         print("Opción inválida.")
